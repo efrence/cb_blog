80054c3cae548e4d7c590f806898581397d54829
Efren Carbajal <ecarbajal@fb.com>
2017-09-27 11:26:13 -0500

    Model Post-Author relationship

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
    before_create :set_time_stamps
    before_save :set_time_stamps
 
+   belongs_to :author, :class_name => "User"
+
   private
   def set_time_stamps
     self.published_at = DateTime.now if self.new_record?
